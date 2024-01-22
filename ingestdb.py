ee42a4056b20acfda7f6fc4ff5ec6598fd36cefd
peekknuf <peekknuf@gmail.com>
2024-01-22 13:34:55 +0100

    fxd env

--- ingestdb.py
+++ ingestdb.py
@@ -2,9 +2,12 @@ import duckdb
 import glob
 import requests
 import os
+from dotenv import load_dotenv
 
-TELEGRAM_BOT_TOKEN = os.environ.get('TELEGRAM_BOT_TOKEN')
-TELEGRAM_CHAT_ID = os.environ.get('TELEGRAM_CHAT_ID')
+
+load_dotenv()
+TELEGRAM_BOT_TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
+TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 
 def send_telegram_message(message):
     url = f'https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage'
@@ -24,10 +27,10 @@ try:
         table_name = f"econ{idx}" 
         conn.execute(f"DROP TABLE IF EXISTS {table_name}")
         conn.execute(f"CREATE TABLE IF NOT EXISTS {table_name} AS FROM read_csv_auto('{csv_file}')")
-        print(f"Table '{table_fname}' created with data from '{csv_file}'") #intentional variable misspelling to proc the telegram message.
+        print(f"Table '{table_123name}' created with data from '{csv_file}'") #intentional variable misspelling to proc the telegram message.
 
 except Exception as e:
     error_message = f"The ingestion process has failed. \nAn error occurred: {str(e)}"
     print(error_message)
-
+    
     send_telegram_message(error_message)
