4f91e5528f43cfbc45c4a044469a8707bcbe1699
peekknuf <peekknuf@gmail.com>
2024-02-21 13:37:37 +0100

    arrays work, yay

--- models/customers_obt.sql
+++ models/customers_obt.sql
@@ -1,9 +1,14 @@
-SELECT customer_name,
-    array(
-        customer_age,
-        email,
-        address,
-        country,
-        phone_number
-    ) as customers_array
-from { { ref('union_all') } }
+WITH customer_data AS (
+  SELECT
+    customer_name,
+    CAST(customer_age AS VARCHAR) AS customer_age,
+    email,
+    address,
+    country,
+    phone_number
+  FROM {{ ref('union_all') }}
+)
+SELECT
+  customer_name,
+  ARRAY[customer_age, email, address, country, phone_number] AS customers_array
+FROM customer_data
--- models/transactions_obt.sql
+++ models/transactions_obt.sql
@@ -1,15 +1,18 @@
-{ { config(materialized = 'table') } }
-SELECT ID,
-    array(
-        timestamp,
-        product_name,
-        price,
-        quantity,
-        revenue,
-        category,
-        payment_method,
-        discount_applied,
-        shipping_method,
-        order_status
-    ) as transactions_array
-FROM { { ref('union_all') } }
+WITH transaction_data AS (
+  SELECT
+    ID,
+    timestamp,
+    product_name,
+    CAST(price AS VARCHAR) AS price,
+    CAST(quantity AS VARCHAR) AS quantity,
+    category,
+    payment_method,
+    discount_applied,
+    shipping_method,
+    order_status
+  FROM {{ ref('union_all') }}
+)
+SELECT
+  ID,
+  ARRAY[timestamp, product_name, price, quantity, category, payment_method, discount_applied, shipping_method, order_status] AS transactions_array
+FROM transaction_data
