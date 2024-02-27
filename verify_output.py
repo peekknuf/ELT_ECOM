9494b21a862731fa0a7fb7863646d9e4b0526482
peekknuf <peekknuf@gmail.com>
2024-02-27 15:47:14 +0100

    models desc

--- verify_output.py
+++ verify_output.py
@@ -3,5 +3,5 @@ import duckdb
 
 conn = duckdb.connect(database="~/ecom.db")
 
-result = conn.execute("SELECT * FROM econ0 LIMIT 10")
+result = conn.execute(query="SELECT * FROM econ0 LIMIT 10")
 print(result.fetchdf())
